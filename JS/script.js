0274dc0adb1840e1e47410d491fe94448e1be017
tshaxrina <tshaxrinan@gmail.com>
2023-09-25 14:08:15 +0500

    first commit

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+let header = document.createElement('header')
+let header_top = document.createElement('div')
+let right = document.createElement('div')
+let location = document.createElement('a')
+let loc_img = document.createElement('img')
+let right_h3 = document.createElement('h3')
+let rright_h3 = document.createElement('h3')
+let center = document.createElement('div')
+let center_text = document.createElement('p')
+let left = document.createElement('div')
+let q_a = document.createElement('a')
+let zakaz = document.createElement('a')
+let lang = document.createElement('a')
+let left_img = document.createElement('img')
+let header_main = document.createElement('div')
+let header_bottom = document.createElement('div')
+let a = document.createElement('a')
+let img = document.createElement('img')
+let h3 = document.createElement('h3')
+let main = document.createElement('main')
+
+for (let i = 1; i <= 5; i++) {
+    let header_bottom_a = document.createElement('a')
+
+    block_div
+}
+
